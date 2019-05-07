fcf52a79bfd43c6731e1dc8a6bb64c5027af60c9
Eric Youngberg <eyoungberg@sasaki.com>
2019-05-07 15:32:41 -0400

    Add docker-compose to application install

--- install/02-applications.sh
+++ install/02-applications.sh
@@ -41,13 +41,17 @@ cd "$CWD"
 rm -rf /tmp/slack_install
 
 
-# Install Docker
+# Install Docker + docker-compose
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt-get update -qq \
 && apt-get install -y docker-ce
-usermod -aG docker "$(logname)"
+usermod -aG docker "$USER"
+
+COMPOSE_PATH=/usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o "$COMPOSE_PATH"
+chmod +x "$COMPOSE_PATH"
 
 
 set +e;
