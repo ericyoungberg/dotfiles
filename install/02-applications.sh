f4d96dcff1de8bd942c0daee764be9a91cb46462
Eric Youngberg <eyoungberg@sasaki.com>
2022-04-12 10:28:22 -0400

    Hmm

--- install/02-applications.sh
+++ install/02-applications.sh
@@ -22,27 +22,6 @@ apt-get install -y \
   vim 
 
 
-# Install WTF dashboard panel
-
-mkdir -p /tmp/wtf_install
-cd /tmp/wtf_install
-wget https://github.com/wtfutil/wtf/releases/download/0.6.0/wtf_0.6.0_linux_amd64.tar.gz
-tar -xzvf wtf_0.6.0_linux_amd64.tar.gz
-cp wtf_0.6.0_linux_amd64/wtf /usr/local/bin/wtf
-cd "$CWD"
-rm -rf /tmp/wtf_install
-
-
-# Install Slack
-
-mkdir -p /tmp/slack_install
-cd /tmp/slack_install
-wget http://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.8-amd64.deb
-dpkg -i slack-desktop-3.3.8-amd64.deb
-cd "$CWD"
-rm -rf /tmp/slack_install
-
-
 # Install Docker + docker-compose
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
